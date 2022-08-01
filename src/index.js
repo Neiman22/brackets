7638f9af7c9981766101cbb4b8d89a5eb415a924
Artem Pushkarev <pushkarev_av@inbox.ru>
2022-08-01 14:16:06 +0300

    add: code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        let OPEN_BRACKETS = [];
+        let BRACKETS_PAIR = {};
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let key = bracketsConfig[i][1];
+            let value = bracketsConfig[i][0];
+            OPEN_BRACKETS.push(value);
+            BRACKETS_PAIR[key] = value;
+        };
+            let stack = [];
+        
+            for (let i = 0; i < str.length; i++) {
+                let currentSymbol = str[i];
+                        if (stack.length !== 0 && BRACKETS_PAIR[currentSymbol] === stack[stack.length - 1]) {
+                            stack.pop();
+                            console.log (stack);
+                        } else {
+                            stack.push(currentSymbol);
+                            console.log (stack);
+                        }
+                
+            }
+            return stack.length === 0;
 }
